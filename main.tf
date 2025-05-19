1063982c8eb2641d3e0954b8030ee3b9b9d7f866
ValentinMalomsoki <159806954+ValentinMalomsoki@users.noreply.github.com>
2025-05-19 11:01:06 +0000

    fix: main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,6 @@
 # The configuration for the `remote` backend.
 terraform {
     backend "remote" {
-        # The name of your Terraform Cloud organization.
-        organization = "example-organization"
-
-        # The name of the Terraform Cloud workspace to store Terraform state files in.
-        workspaces {
-        name = "example-workspace"
-        }
     }
 }
 
